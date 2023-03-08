5a212531c8b6dbb704b6f71c69c695d85431ad82
sanity_nil <d.2510086@gmail.com>
2023-03-08 10:55:26 +0100

    Deleted redundant order-service

--- OrderService/src/main/java/com/sanitynil/orderservice/domain/order/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sanitynil.orderservice.domain.order.repository;
-
-import com.sanitynil.orderservice.domain.order.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<Order, Integer> {
-}
--- OrderService/src/main/java/com/sanitynil/orderservice/infra/util/PaymentStatus.java
+++
@@ -1,7 +0,0 @@
-package com.sanitynil.orderservice.infra.util;
-
-public enum PaymentStatus {
-    STATUS_PAID,
-    STATUS_FAILED,
-    STATUS_PENDING,
-}
